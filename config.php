792034bb637979d4ef8d56b4850eb0daa0a8c8a4
John Doe <111336092+SwagAbyss@users.noreply.github.com>
2023-07-31 20:28:48 +0530

    commit

--- config.php
+++ config.php
@@ -8,5 +8,5 @@ $conn = mysqli_connect($servername, $username, $password, "$dbname");
 if (!$conn) {
     die('Could not Connect MySql Server:' . mysqli_connect_error());
 } else {
-    // echo "Connected";
+    echo "Connected";
 }
