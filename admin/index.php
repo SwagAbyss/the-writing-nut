377584ef3589a8b448514ea8b7f9af40cd9110f1
John Doe <111336092+SwagAbyss@users.noreply.github.com>
2023-07-31 20:16:22 +0530

    commit

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,7 @@
+<?php
+$base_url = "http://www.thewritingnut.in/admin/";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,6 +22,8 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
+  <link href="<?php echo $base_url; ?>./assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
   <link href="/admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="/admin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="/admin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
@@ -25,6 +31,8 @@
   <link href="/admin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="/admin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="/admin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+
 
   <!-- Template Main CSS File -->
   <link href="/admin/assets/css/style.css" rel="stylesheet">
@@ -114,6 +122,9 @@
   <!-- End #main -->
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
   <!-- Vendor JS Files -->
+
+  <script src="<?php echo $base_url; ?>./assets/vendor/apexcharts/apexcharts.min.js"></script>
+
   <script src="/admin/assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="/admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="/admin/assets/vendor/chart.js/chart.umd.js"></script>
