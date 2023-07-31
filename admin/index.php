8b002d1ceae60d1d549ac4541e28d70cb0a99641
John Doe <111336092+SwagAbyss@users.noreply.github.com>
2023-07-31 20:39:33 +0530

    commit

--- admin/index.php
+++ admin/index.php
@@ -80,7 +80,6 @@ $base_url = "http://www.thewritingnut.in/admin/";
                     <div class="col-12">
                       <label for="yourUsername" class="form-label">Username</label>
                       <div class="input-group has-validation">
-                        <span class="input-group-text" id="inputGroupPrepend">@</span>
                         <input type="text" name="uname" class="form-control" id="yourUsername" required>
                         <div class="invalid-feedback">Please enter your username.</div>
                       </div>
