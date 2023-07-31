93cb2403974bee04f41ed48ac3aa892cec9c3418
John Doe <111336092+SwagAbyss@users.noreply.github.com>
2023-07-31 21:59:59 +0530

    commit

--- norm.php
+++ norm.php
@@ -10,8 +10,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "INSERT INTO writingnut_sub (date, time, email) VALUES ('23-01-2023', '01:52:00', 'johnny@example.com')";
+$email = 'swag@gmail.com';
+$sql = "INSERT INTO writingnut_sub (date, time, email) VALUES ('23-01-2023', '01:52:00', '$email')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
