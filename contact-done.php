4f2a7c8f697842015cfbf4be34da6ff4385e85ef
sathish p <sathish8608855@gmail.com>
2023-07-31 21:26:53 +0530

    Update contact-done.php

--- contact-done.php
+++ contact-done.php
@@ -195,6 +195,27 @@
         </label>
     </div>
     <!-- Dark version -->
+   
+    <?php
+        include_once 'admin/config.php';
+        if (isset($_POST['submit'])) {
+            $date = date("d-m-Y");
+            $time = date("h:i");
+            $name = mysqli_real_escape_string($conn, $_POST['con_name']);
+            $email = mysqli_real_escape_string($conn, $_POST['con_email']);
+            $phone = mysqli_real_escape_string($conn, $_POST['con_phone']);
+            $subject = mysqli_real_escape_string($conn, $_POST['con_subject']);
+            $message = mysqli_real_escape_string($conn, $_POST['con_message']);
+            $sql = "INSERT INTO writingnut_contact (date,time,name,email,phone,subject,message)
+    VALUES ('$date','$time','$name','$email','$phone','$subject', '$message')";
+            if (mysqli_query($conn, $sql)) {
+                // echo "New record has been added successfully !";
+            } else {
+                echo "Error: " . $sql . ":-" . mysqli_error($conn);
+            }
+            mysqli_close($conn);
+        }
+        ?>
 
     <!-- Links of JS files -->
     <script src="assets/js/jquery.min.js"></script>
