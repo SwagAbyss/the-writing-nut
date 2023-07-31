217b698fc5b4519699f875c3878a4b7c7a1f570a
John Doe <111336092+SwagAbyss@users.noreply.github.com>
2023-07-31 22:33:07 +0530

    commit

--- subscribe-done.php
+++ subscribe-done.php
@@ -180,6 +180,10 @@
         $conn->close();
     }
     ?>
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
 
     <!-- Start Footer Area -->
     <footer class="footer-area footer-style-wrap">
