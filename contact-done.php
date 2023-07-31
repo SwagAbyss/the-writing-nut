c18ae557a0cc727111b6b3842e7e22f92303dfe6
John Doe <111336092+SwagAbyss@users.noreply.github.com>
2023-07-31 21:11:29 +0530

    commit

--- contact-done.php
+++ contact-done.php
@@ -145,27 +145,20 @@
     <?php
     include_once 'admin/config.php';
     if (isset($_POST['submit'])) {
-        try {
-            $date = date("d-m-Y");
-            $time = date("h:i");
-            $name = mysqli_real_escape_string($conn, htmlspecialchars($_POST['con_name']));
-            $email = mysqli_real_escape_string($conn, htmlspecialchars($_POST['con_email']));
-            $phone = mysqli_real_escape_string($conn, htmlspecialchars($_POST['con_phone']));
-            $subject = mysqli_real_escape_string($conn, htmlspecialchars($_POST['con_subject']));
-            $message = mysqli_real_escape_string($conn, htmlspecialchars($_POST['con_message']));
-
-            $sql = "INSERT INTO writingnut_contact (date,time,name,email,phone,subject,message)
+        $date = date("d-m-Y");
+        $time = date("h:i");
+        $name = $_POST['con_name'];
+        $email = $_POST['con_email'];
+        $phone = $_POST['con_phone'];
+        $subject = $_POST['con_subject'];
+        $message = $_POST['con_message'];
+        $sql = "INSERT INTO writingnut_contact (date,time,name,email,phone,subject,message)
         VALUES ('$date','$time','$name','$email','$phone','$subject', '$message')";
-
-            if (mysqli_query($conn, $sql)) {
-                // echo "New record has been added successfully !";
-            } else {
-                throw new Exception("Error: " . $sql . ":-" . mysqli_error($conn));
-            }
-        } catch (Exception $e) {
-            echo "Error: " . $e->getMessage();
+        if (mysqli_query($conn, $sql)) {
+            // echo "New record has been added successfully !";
+        } else {
+            echo "Error: " . $sql . ":-" . mysqli_error($conn);
         }
-
         mysqli_close($conn);
     }
     ?>
