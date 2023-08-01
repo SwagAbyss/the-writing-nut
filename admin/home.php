a69fcb1b2a48a98a7100b0f79844baee1e286e31
John Doe <111336092+SwagAbyss@users.noreply.github.com>
2023-08-01 14:13:04 +0530

    commit

--- admin/home.php
+++ admin/home.php
@@ -100,7 +100,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
               <li>
                 <a class="dropdown-item d-flex align-items-center" href="./log_logout.php">
                   <i class="bi bi-box-arrow-right"></i>
-                  <span>Sign Out</span>
+                  <span>Log Out</span>
                 </a>
               </li>
             </ul><!-- End Profile Dropdown Items -->
