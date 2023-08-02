66daba4dd2029910c97e4e7ed7a90e7c0de263c6
sathish p <sathish8608855@gmail.com>
2023-08-02 21:15:49 +0530

    done

--- admin/subscribe.php
+++ admin/subscribe.php
@@ -44,7 +44,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
     <header id="header" class="header fixed-top d-flex align-items-center">
 
       <div class="d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+        <a href="#" class="logo d-flex align-items-center">
           <img src="../assets/img/original_img/logo.jpeg" alt="">
           <span class="d-none d-lg-block">The Writing Nut</span>
         </a>
